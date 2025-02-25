1088fdbf849303c3c3486bf9dd583b97eb2b2e50
yinxinyu <1296822630@qq.com>
2025-02-26 01:58:43 +0800

    【Unity】-【YooAsset初始化设置】

--- Assets/Scripts/Managers/BundleManagerYooAsset/BundleManagerYooAsset.cs
+++ Assets/Scripts/Managers/BundleManagerYooAsset/BundleManagerYooAsset.cs
@@ -28,7 +28,7 @@ namespace Managers
         /// </summary>
         private static Dictionary<string, string> _AssetDic { get; set; } = new();
 
-        private ResourcePackage _package;
+        public ResourcePackage _Package { get; set; }
 
         public IEnumerator InitResourceList()
         {
@@ -46,15 +46,40 @@ namespace Managers
             yield return null;
         }
 
-        public IEnumerator InitPackage()
+        public IEnumerator InitYooAssets()
         {
             //初始化资源系统
             YooAssets.Initialize();
 
             //设置默认Package
-            _package = YooAssets.CreatePackage("TestPackage");
-            YooAssets.SetDefaultPackage(_package);
+            _Package = YooAssets.CreatePackage("TestPackage");
+            YooAssets.SetDefaultPackage(_Package);
 
+            //初始化Package
+            yield return StartCoroutine(InitPackageOffline());
+            //初始化Package版本信息
+            yield return StartCoroutine(InitPackageVersion());
+            //初始化资源列表
+            yield return StartCoroutine(InitResourceList());
+
+            
+            //
+            // //更新资源清单
+            // var operation2 = _Package.UpdatePackageManifestAsync(packageVersion);
+            // yield return operation2;
+            //
+            // if (operation2.Status == EOperationStatus.Succeed)
+            //     //更新成功
+            //     LogTools.Log("资源更新成功！");
+            // else
+            //     //更新失败
+            //     LogTools.LogError(operation2.Error);
+
+            
+        }
+
+        private IEnumerator InitPackageOffline()
+        {
             //初始化本地Package
             // ReSharper disable once IdentifierTypo
             var buildinFileSystemParams = FileSystemParameters.CreateDefaultBuildinFileSystemParameters();
@@ -62,50 +87,39 @@ namespace Managers
             {
                 BuildinFileSystemParameters = buildinFileSystemParams
             };
-            var initOperation = _package.InitializeAsync(initParameters);
+            var initOperation = _Package.InitializeAsync(initParameters);
             yield return initOperation;
 
             if (initOperation.Status == EOperationStatus.Succeed)
                 LogTools.Log("资源包初始化成功！");
             else
                 LogTools.LogError($"资源包初始化失败：{initOperation.Error}");
+        }
 
+        private IEnumerator InitPackageVersion()
+        {
             //更新资源版本
-            var operation1 = _package.RequestPackageVersionAsync();
-            yield return operation1;
+            var request = _Package.RequestPackageVersionAsync();
+            yield return request;
 
-            var packageVersion = "";
-            if (operation1.Status == EOperationStatus.Succeed)
+            if (request.Status == EOperationStatus.Succeed)
             {
                 //更新成功
-                packageVersion = operation1.PackageVersion;
-                LogTools.Log($"Request package Version : {packageVersion}");
+                var packageVersion = request.PackageVersion;
+                LogTools.Log($"请求Package版本成功！【{packageVersion}】");
             }
             else
             {
                 //更新失败
-                LogTools.LogError(operation1.Error);
+                LogTools.LogError($"请求Package版本失败！{request.Error}");
             }
-
-            //更新资源清单
-            var operation2 = _package.UpdatePackageManifestAsync(packageVersion);
-            yield return operation2;
-
-            if (operation2.Status == EOperationStatus.Succeed)
-                //更新成功
-                LogTools.Log("资源更新成功！");
-            else
-                //更新失败
-                LogTools.LogError(operation2.Error);
-
-            yield return StartCoroutine(InitResourceList());
         }
 
         public AssetHandle LoadAssetAsync<T>(string assetName) where T : Object
         {
             if (_AssetDic.TryGetValue(assetName, out var value))
             {
-                var handle = _package.LoadAssetAsync<T>(value);
+                var handle = _Package.LoadAssetAsync<T>(value);
                 return handle;
             }
 
--- Assets/Scripts/Managers/GameManager.cs
+++ Assets/Scripts/Managers/GameManager.cs
@@ -1,4 +1,5 @@
 using System.Collections;
+using UI;
 using UnityEngine;
 using YooAsset;
 
@@ -6,26 +7,48 @@ namespace Managers
 {
     public class GameManager : MonoSingleton<GameManager>
     {
+        private GameObject _dialogLoading = null;
+        
         protected override void SingletonAwake()
         {
             base.SingletonAwake();
             StartCoroutine(InitImportance());
+            
+
+            
+            
         }
 
         //启动重要的程序
         private IEnumerator InitImportance()
         {
-            yield return StartCoroutine(InitYooAsset()) ;
+            yield return StartCoroutine(BundleManagerYooAsset._Instance.InitYooAssets()) ;
+            
+            //启动YooAssets后先将loading界面加载出来
+            yield return StartCoroutine(LoadLoading());
+        }
+
+        private IEnumerator LoadLoading()
+        {
+            var handle = BundleManagerYooAsset._Instance.LoadAssetAsync<DialogLoading>(nameof(DialogLoading));
+            yield return handle;
+            if (handle.Status != EOperationStatus.Succeed)
+            {
+                LogTools.LogError("DialogLoading加载失败！");
+                yield break;
+            }
+
+            _dialogLoading = handle.InstantiateSync(GameObject.Find("Canvass").transform);
         }
         
         private IEnumerator InitYooAsset()
         {
             //初始化资源系统
             YooAssets.Initialize();
-            yield return null;
+            
             //设置默认Package
-            // _package = YooAssets.CreatePackage("TestPackage");
-            // YooAssets.SetDefaultPackage(_package);
+            BundleManagerYooAsset._Instance._Package = YooAssets.CreatePackage("TestPackage");
+            YooAssets.SetDefaultPackage(BundleManagerYooAsset._Instance._Package);
 
             // _loading.gameObject.SetActive(true);
             // yield return BundleManagerYooAsset._Instance.InitPackage();
@@ -33,6 +56,8 @@ namespace Managers
             // AssetHandle handle = BundleManagerYooAsset._Instance.LoadAssetAsync<GameObject>("TestObj");
             // yield return handle;
             // handle.InstantiateSync(_canvas.transform);
+            
+            yield return null;
         }
     }
 }
